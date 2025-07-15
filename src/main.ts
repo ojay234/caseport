85bd28ee50fcab054f2ae41f1a5efc1452dcda6d
David Ochuma <davidace998@gmail.com>
2025-07-15 10:43:47 +0100

    resolve cors

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+
+
+    const app = await NestFactory.create<NestExpressApplication>(AppModule);
+    
+  app.enableCors({
+  origin: ['http://localhost:5173',], 
+  credentials: true, 
+});
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    })
+  );
 
   const config = new DocumentBuilder()
     .setTitle('CasePort API')

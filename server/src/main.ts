974079b139b96655532b798da71717c070b9ef71
David Ochuma <davidace998@gmail.com>
2025-09-16 16:05:44 +0100

    added frontend url to origin

--- server/src/main.ts
+++ server/src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
     const app = await NestFactory.create<NestExpressApplication>(AppModule);
     
   app.enableCors({
-  origin: ['http://localhost:5173',], 
+  origin: ['http://localhost:5173', 'https://caseport-frontend.onrender.com/'], 
   credentials: true, 
 });
 
